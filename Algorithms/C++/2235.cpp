960927a022c76944514c6c4293b03cbc100f8a64
Naif al-Sahabi <2210002362@iau.edu.sa>
2023-04-20 09:57:53 +0300

    Create 2235.cpp

---
+++ Algorithms/C++/2235.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define Nc4    \
+    cin.tie(0);  \
+    cout.tie(0); \
+    ios::sync_with_stdio(0);
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+ 
+int main(int argc, char const *argv[])
+{
+    Nc4;
+
+    int n1 , n2;
+    cin >> n1 >> n2;
+
+    cout<<n1+n2;
+     
+}
